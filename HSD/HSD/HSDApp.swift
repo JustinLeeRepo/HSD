41a0257abdb171e619ad75579ebcc677d687c1a6
Justin Lee <justinj.lee23@gmail.com>
2025-08-25 14:10:04 -0700

    Proj init

---
+++ HSD/HSD/HSDApp.swift
@@ -0,0 +1,17 @@
+//
+//  HSDApp.swift
+//  HSD
+//
+//  Created by Justin Lee on 8/25/25.
+//
+
+import SwiftUI
+
+@main
+struct HSDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
